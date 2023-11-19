881fd54293767ac64fe519f0f3d9b676c35b8437
6Syslik9 <147944722+6Syslik9@users.noreply.github.com>
2023-11-19 18:12:54 +0200

    Create Lab2.js
    Lab2

---
+++ Lab2.js
@@ -0,0 +1,121 @@
+// Ваш компонент Square
+function Square({ value, onSquareClick, isWinningSquare }) {
+  return (
+    <button
+      className={`square ${isWinningSquare ? 'winning-square' : ''}`}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+// Измененная функция calculateWinner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return { winner: squares[a], line: [a, b, c] };
+    }
+  }
+  return null;
+}
+
+// Ваш компонент Board
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  const winnerInfo = calculateWinner(squares);
+  let status;
+  let winningSquares = [];
+
+  if (winnerInfo) {
+    status = 'Winner: ' + winnerInfo.winner;
+    winningSquares = winnerInfo.line;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {Array.from({ length: 3 }, (_, index) => (
+          <Square
+            key={index}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+            isWinningSquare={winningSquares.includes(index)}
+          />
+        ))}
+      </div>
+      <div className="board-row">
+        {/* Аналогично для других строк... */}
+      </div>
+    </>
+  );
+}
+
+// Ваш компонент Game
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
